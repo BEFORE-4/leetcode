719b0b2ca439c5f215a0f8a9f8eaa895958da9fa
BEFORE-4 <836735557@qq.com>
2019-04-11 12:27:00 +0800

    判断是否为回文
    回文判断

---
+++ src/chap0/Solution9.java
@@ -0,0 +1,13 @@
+package chap0;
+
+public class Solution9 {
+    public boolean isPalindrome(int x) {
+        String str = Integer.valueOf(x).toString();
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++){
+            if (chars[i] != chars[chars.length - i - 1])
+                return false;
+        }
+        return true;
+    }
+}
